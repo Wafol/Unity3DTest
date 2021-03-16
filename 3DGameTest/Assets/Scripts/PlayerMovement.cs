c2e69b7dfdca502e5f3a774ae45b0701bb561e9f
Wafol <pavel.krauz1@gmail.com>
2021-03-16 15:26:18 +0100

    tutorial 03

--- 3DGameTest/Assets/Scripts/PlayerMovement.cs
+++ 3DGameTest/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,34 @@ public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody rb;
 
+    public float forwardForce = 500f;
+    public float sidewaysForce = 1000f;
+
+    private bool goLeft;
+    private bool goRight;
+
     // Start is called before the first frame update
     // ReSharper disable once ArrangeTypeMemberModifiers
-    void Start() {
+    public void Start() {
         Debug.Log("hello work");
+        //rb.AddForce(0, 0, 90);
+    }
+
+    public void Update() {
+        //input
+        goRight = Input.GetKey("d");
+        goLeft = Input.GetKey("a");
     }
 
     // Update is called once per frame
     // ReSharper disable once ArrangeTypeMemberModifiers
-    void FixedUpdate() {
-        rb.AddForce(0, 0, Time.deltaTime * 800);
+    public void FixedUpdate() {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (goRight)
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+
+        if (goLeft)
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
     }
 }
